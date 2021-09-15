c2f0789703f14d1a96fc181d548f92525dcf8dd4
Mothilal Rajendira Prasath <78716398+mothilalrajendiraprasath@users.noreply.github.com>
2021-09-15 09:15:51 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,13 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("NO OF PLAYERS:");
-        Scanner s = new Scanner(System.in);
-        int a = s.nextInt();
-        if ( a == 1)
-            System.out.println("Starting at the position 0");
-        else
-            System.out.println("Excessive player error");
-
+        System.out.println("Snake and ladder:");
+       int startposition=0;
     }
 }
